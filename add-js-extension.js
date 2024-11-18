f5f4b1eb301a6a8f057c7fbf48f322df6aa8c656
Ed Wright <ewright@digitalfutures.com>
2024-11-18 14:36:15 +0000

    FIX: file extensions on build

---
+++ add-js-extension.js
@@ -0,0 +1,20 @@
+import { readdirSync, lstatSync, readFileSync, writeFileSync } from "fs";
+import { join, extname } from "path";
+
+function addJsExtension(dir) {
+    readdirSync(dir).forEach((file) => {
+        const fullPath = join(dir, file);
+        if (lstatSync(fullPath).isDirectory()) {
+            addJsExtension(fullPath);
+        } else if (extname(fullPath) === ".js") {
+            let content = readFileSync(fullPath, "utf8");
+            content = content.replace(
+                /(import .* from ['"])(.*)(['"])/g,
+                "$1$2.js$3"
+            );
+            writeFileSync(fullPath, content, "utf8");
+        }
+    });
+}
+
+addJsExtension(join(process.cwd(), "dist"));

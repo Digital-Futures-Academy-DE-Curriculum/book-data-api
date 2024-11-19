33348a0f6238cb4cf49e0e2d0cb2ca46bc862aff
Ed Wright <ewright@digitalfutures.com>
2024-11-19 16:16:03 +0000

    FIX: make path to public/index.html relative to root

--- src/controllers/BooksToScrape.controller.ts
+++ src/controllers/BooksToScrape.controller.ts
@@ -7,14 +7,13 @@ import ErrorMessages from "../constants/ErrorMessages.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const HOME_PAGE_PATH = "../../public/index.html";
+const HOME_PAGE_PATH = path.join(process.cwd(), "public", "index.html");
 
 export default class BooksToScrapeController {
     getHomePage: (req: Request, res: Response) => void = async (req, res) => {
-        const filePath = path.join(__dirname, HOME_PAGE_PATH);
         try {
-            await fs.access(filePath, fs.constants.F_OK);
-            res.sendFile(filePath);
+            await fs.access(HOME_PAGE_PATH, fs.constants.F_OK);
+            res.sendFile(HOME_PAGE_PATH);
         } catch {
             res.status(404).send(ErrorMessages.FILE_NOT_FOUND);
         }
